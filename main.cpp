47603ee5455d4f748b40c95f9fc597c5fe306e57
IAmBeli <felix.syndicate@mail.ru>
2026-03-16 18:40:43 +0800

    Add Mage and Warrior classes with polymorphism

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 #include<ctime>
 #include<cstdlib>
 using namespace std;
-class Player{
-private:
+class Character{
+protected:
     string name;
     int health;
     int damage;
 public:
-    Player(string n, int h, int d) : name(n), health(h), damage(d) {}
-    void takeDamage(int amount){
+    Character(string n, int h, int d) : name(n), health(h), damage(d) {}
+    virtual void takeDamage(int amount){
         health -= amount;
         if(health < 0) health = 0;
         cout << name << " took " << amount << " damage. HP left: " << health << endl;
@@ -26,7 +26,7 @@ public:
         cout << "HP: " << health << endl;
         cout << "DMG: " << damage << endl;
     }
-    void attack(Player& other){
+    virtual void attack(Character& other){
         int randomDamage = (damage - 5) + (rand() % 11);
         if((rand() % 10) < 2){
             randomDamage *= 2;
@@ -42,10 +42,43 @@ public:
     int getHealth()const{return health;}
     int getDamage()const{return damage;}
 };
+class Mage : public Character{
+private:
+    int mana;
+public:
+    Mage(string n, int h, int d, int m) : Character(n, h, d), mana(m) {}
+    void attack(Character& other)override{
+        if(mana >= 10){
+            cout << name << " casts a FIREBALL at " << other.getName() << "!" << endl;
+            int magicDamage = damage * 2;
+            other.takeDamage(magicDamage);
+            mana -= 10;
+            cout << "Mana left: " << mana << endl; 
+        }else{
+            cout << "Not enough mana! " << name << " hits weakly with a staff" << endl;
+            Character::attack(other);
+        }
+    }
+};
+class Warrior : public Character{
+private:
+    int armor;
+public:
+    Warrior(string n, int h, int d, int a) : Character(n, h, d), armor(a){}
+    void takeDamage(int amount)override{
+        amount -= armor;
+        if(amount < 0){
+            cout << "Armor saved " << name << " from damage!" << endl;
+        }else{
+            Character::takeDamage(amount);
+        }
+    }
+};
 int main(){
     srand(time(0));
-    Player hero("Beli", 100, 20);
-    Player enemy("Goblin", 50, 10);
+    Mage hero("Beli", 100, 20, 30);
+    Warrior enemy("Goblin", 120, 15, 5);
+    cout << "-- Battle begins --" << endl;
     while(hero.isAlive() && enemy.isAlive()){
         hero.attack(enemy);
         if(enemy.isAlive()){
@@ -57,4 +90,5 @@ int main(){
     }else if(hero.isAlive() == 0){
         cout << "Enemy won this fight!" << endl;
     }
+    return 0;
 }
